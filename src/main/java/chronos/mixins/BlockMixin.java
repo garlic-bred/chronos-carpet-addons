bda1cae6be8586bc05591588d1350c9df8beb49a
bread <garlicbred69@gmail.com>
2024-01-01 19:56:07 -0700

    fixed better careful break not working properly

--- src/main/java/chronos/mixins/BlockMixin.java
+++ src/main/java/chronos/mixins/BlockMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Util;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,7 +23,18 @@ public class BlockMixin {
 
     @Inject(method = "dropStack(Lnet/minecraft/world/World;Ljava/util/function/Supplier;Lnet/minecraft/item/ItemStack;)V", at = @At("HEAD"), cancellable = true)
     private static void captureDrops(World world, Supplier<ItemEntity> itemEntitySupplier, ItemStack stack, CallbackInfo ci) {
-        if (CapturedDrops.isCapturing()) {
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        boolean fromPlayerAction = false;
+        for (StackTraceElement element : trace) {
+//            try {
+//                CapturedDrops.getPlayer().sendSystemMessage(new LiteralText(element.getClassName()), Util.NIL_UUID);
+//            } catch (Exception e) { /* ignore exception */ }
+            if (element.getClassName().equals("net.minecraft.class_3225")) {
+                fromPlayerAction = true;
+                break;
+            }
+        }
+        if (fromPlayerAction && CapturedDrops.isCapturing() && CapturedDrops.getPlayer().getWorld().getServer().isOnThread()) {
             if (CapturedDrops.capture(stack)) {
                 PlayerEntity player = CapturedDrops.getPlayer();
                 world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2f, (CarpetServer.rand.nextFloat() - CarpetServer.rand.nextFloat()) * 1.4F + 2.0F);
