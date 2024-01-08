d12edd1814fba3749c3c674c6a1117460883d832
bread <garlicbred69@gmail.com>
2024-01-08 09:45:17 -0700

    disabled betterCarefulBreak as it was not working properly on multiplayer servers - needs a rewrite

--- src/main/java/chronos/ChronosSettings.java
+++ src/main/java/chronos/ChronosSettings.java
@@ -8,11 +8,11 @@ public class ChronosSettings
 {
     private static final String CHRONOS = "chronos";
 
-    @Rule(
-            desc = "If you break a block while sneaking then the block gets put in your inventory.",
-            extra = {"Any blocks supported by that block that get broken instantly will also be put in your inventory."},
-            category = { SURVIVAL, EXPERIMENTAL, CHRONOS }
-    )
+//    @Rule(
+//            desc = "If you break a block while sneaking then the block gets put in your inventory.",
+//            extra = {"Any blocks supported by that block that get broken instantly will also be put in your inventory."},
+//            category = { SURVIVAL, EXPERIMENTAL, CHRONOS }
+//    )
     public static boolean betterCarefulBreak = false;
 
     @Rule(
