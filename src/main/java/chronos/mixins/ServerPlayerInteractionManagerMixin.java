cdcf5c01b247edfa3ea4045352e249e7f54395bc
bread <garlicbred69@gmail.com>
2024-04-09 17:18:34 -0700

    added a rule to change the max block reach distance

--- src/main/java/chronos/mixins/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/chronos/mixins/ServerPlayerInteractionManagerMixin.java
@@ -1,15 +1,15 @@
 package chronos.mixins;
 
 import chronos.ChronosSettings;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerPlayNetworkHandler.class)
-public abstract class ServerPlayNetworkHandlerMixin {
+@Mixin(ServerPlayerInteractionManager.class)
+public class ServerPlayerInteractionManagerMixin {
 
-    @Redirect(method = "onPlayerInteractBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
+    @Redirect(method = "processBlockBreakingAction", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
     private double modifyReachDistance() {
         return ChronosSettings.maxBlockReachDistance * ChronosSettings.maxBlockReachDistance;
     }
